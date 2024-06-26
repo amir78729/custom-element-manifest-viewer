a1d6e4fec5b3aa587e97ab13dace994366b85afb
Amirhossein Alibakhshi <amir78729@gmail.com>
2024-06-26 18:24:50 +0330

    fix: change manifest file path in dev and prod

--- src/custom-element-manifest-viewer/custom-element-manifest-viewer.stories.ts
+++ src/custom-element-manifest-viewer/custom-element-manifest-viewer.stories.ts
@@ -31,6 +31,10 @@ import "@tapsioss/web-components/dist/tooltip";
 import "@tapsioss/web-components/dist/button";
 import "@tapsioss/web-components/dist/styles/theme.css";
 
+const isDev =  process.env.NODE_ENV === 'development';
+
+const manifestSource = isDev ? '../sample-custom-elements.json' : '../custom-element-manifest-viewer/sample-custom-elements.json'
+
 export default {
   title: 'custom-element-manifest-viewer',
   component: 'custom-element-manifest-viewer',
@@ -90,7 +94,7 @@ interface ArgTypes {
 }
 
 const Template: Story<ArgTypes> = ({ tagName}) => html`
-  <custom-element-manifest-viewer tag-name=${tagName} src='../custom-element-manifest-viewer/sample-custom-elements.json'>
+  <custom-element-manifest-viewer tag-name=${tagName} src=${manifestSource}>
   </custom-element-manifest-viewer>
 `;
 
@@ -102,7 +106,7 @@ Default.args = {
 };
 
 const Slots: Story<ArgTypes> = ({ tagName}) => html`
-  <custom-element-manifest-viewer tag-name='tap-row' src='../custom-element-manifest-viewer/sample-custom-elements.json'>
+  <custom-element-manifest-viewer tag-name='tap-row' src=${manifestSource}>
     <tap-icon-default color="black" data-knob-type="slot" slot="leading" title="icon"></tap-icon-default>
     <tap-checkbox data-knob-type="slot" slot="leading" title="checkbox"></tap-checkbox>
     <tap-radio data-knob-type="slot" slot="leading" title="radio"></tap-radio>
@@ -122,7 +126,6 @@ export const CustomSlots = Slots.bind({});
 CustomSlots.args = {
   tagName: 'tap-avatar'
 };
-
 
 const Theme: Story<ArgTypes> = ({ tagName}) => html`
   <style>
@@ -194,7 +197,7 @@ const Theme: Story<ArgTypes> = ({ tagName}) => html`
       border-radius: 0 0 8px 8px;
     }
   </style>
-  <custom-element-manifest-viewer tag-name=${tagName} src='../custom-element-manifest-viewer/sample-custom-elements.json'>
+  <custom-element-manifest-viewer tag-name=${tagName} src=${manifestSource}>
   </custom-element-manifest-viewer>
 `;
 
